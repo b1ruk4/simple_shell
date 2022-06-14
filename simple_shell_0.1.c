0799442088e218b86c3b010af906d3a22ad0ba38
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-06-14 22:02:43 +0300

    both 0.1

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -98,3 +98,67 @@ return ((*builtin_func[i])(args));
 }
 return (_launch(args));
 }
+/**
+ * Read a line of input from stdin.
+ * return The line from stdin.
+ */
+char *_read_line(void)
+{
+#ifdef bash_USE_STD_GETLINE
+char *line = NULL;
+ssize_t bufsize = 0;
+
+if (getline(&line, &bufsize, stdin) == -1)
+{
+if (feof(stdin))
+{
+exit(EXIT_SUCCESS);
+}
+else
+{
+perror("bash: getline\n");
+exit(EXIT_FAILURE);
+}
+}
+return (line);
+#else
+#define bash_RL_BUFSIZE 1024
+int bufsize = bash_RL_BUFSIZE;
+int position = 0;
+char *buffer = malloc(sizeof(char) * bufsize);
+int c;
+if (!buffer)
+{
+fprintf(stderr, "bash: allocation error\n");
+exit(EXIT_FAILURE);
+}
+while (1)
+{
+c = getchar();
+if (c == EOF)
+{
+exit(EXIT_SUCCESS);
+}
+else if (c == '\n')
+{
+buffer[position] = '\0';
+return (buffer);
+}
+else
+{
+buffer[position] = c;
+}
+position++;
+if (position >= bufsize)
+{
+bufsize += bash_RL_BUFSIZE;
+buffer = realloc(buffer, bufsize);
+if (!buffer)
+{
+fprintf(stderr, "bash: allocation error\n");
+exit(EXIT_FAILURE);
+}
+}
+}
+#endif
+}
