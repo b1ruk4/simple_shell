b128ebc7ffe4edd9c75d2369d44669948fe45d26
b1ruk4 <birukwondimubkk@gmail.com>
2022-06-14 12:06:02 -0700

    split line

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -162,3 +162,42 @@ exit(EXIT_FAILURE);
 }
 #endif
 }
+#define bash_TOK_BUFSIZE 64
+#define bash_TOK_DELIM " \t\r\n\a"
+/**
+  * Split a line into tokens (very naively).
+  * param line The line.
+  * return Null-terminated array of tokens.
+ */
+char **_split_line(char *line)
+{
+int bufsize = bash_TOK_BUFSIZE, position = 0;
+char **tokens = malloc(bufsize *sizeof(char *));
+char *token, **tokens_backup;
+if (!tokens)
+{
+fprintf(stderr, "bash: allocation error\n");
+exit(EXIT_FAILURE);
+}
+token = strtok(line, bash_TOK_DELIM);
+while (token != NULL)
+{
+tokens[position] = token;
+position++;
+if (position >= bufsize)
+{
+bufsize += bash_TOK_BUFSIZE;
+tokens_backup = tokens;
+tokens = realloc(tokens, bufsize *sizeof(char *));
+if (!tokens)
+{
+free(tokens_backup);
+fprintf(stderr, "bash: allocation error\n");
+exit(EXIT_FAILURE);
+}
+}
+token = strtok(NULL, bash_TOK_DELIM);
+}
+tokens[position] = NULL;
+return (tokens);
+}
