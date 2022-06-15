2cbf142a9999fa22b9c259c550995e1cf3fe2ee3
b1ruk4 <birukwondimubkk@gmail.com>
2022-06-15 10:55:49 -0700

    main.c function

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - reads commands and executes
+ * argc Argument count.
+ * param argv Argument vector.
+ * Return: return status code
+ */
+int main(void)
+{
+_loop();
+return (EXIT_SUCCESS);
+}
+
