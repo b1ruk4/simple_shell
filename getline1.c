cfa0b839196299a9d1c6fa68cb2d6f5d36c3e36a
b1ruk4 <birukwondimubkk@gmail.com>
2022-06-16 15:05:38 -0700

    getline

--- getline1.c
+++ getline1.c
@@ -7,38 +7,38 @@
  */
 int _getline(char **lineptr, int fd)
 {
-    int size = 1025;
-	int old_size = 0;
-	int r = 1;
-	int sum = 0;
-	static char buffer[1025];
-	static int begin;
-	static int end;
-	int c = 0;
-	int d;
+int size = 1025;
+int old_size = 0;
+int r = 1;
+int sum = 0;
+static char buffer[1025];
+static int begin;
+static int end;
+int c = 0;
+int d;
 
-    while (1)
-	{
-		if (begin == end)
-		{
-			while (sum < 1024 && r != 0)
-			{
-				r = read(fd, buffer + sum, 1024 - sum);
-				begin = 0;
-				sum += r;
-				end = sum;
-				/*printf("r : %d\n", r);*/
-				for (d = 0; r != 0 && d < end; d++)
-					if (buffer[d] == '\n')
-						r = 0;
-			}
-			if (sum == 0)
-			{
-				buffer[0] = 0;
-				return (sum);
-			}
-			buffer[sum] = 0;
-			sum = 0;
-		}
-    }
+while (1)
+{
+if (begin == end)
+{
+while (sum < 1024 && r != 0)
+{
+r = read(fd, buffer + sum, 1024 - sum);
+begin = 0;
+sum += r;
+end = sum;
+/*printf("r : %d\n", r);*/
+for (d = 0; r != 0 && d < end; d++)
+if (buffer[d] == '\n')
+r = 0;
+}
+if (sum == 0)
+{
+buffer[0] = 0;
+return (sum);
+}
+buffer[sum] = 0;
+sum = 0;
+}
+}
 }
